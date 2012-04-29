ab508d69c6ac2fda2dbad7b6caf6412bffbc4449
Jack Reilly <jackdreilly@gmail.com>
2012-04-29 16:40:39 -0700

    scrapped unused

--- weight/require_me.rb
+++
@@ -1,9 +0,0 @@
-require './lib/graph.rb'
-require './lib/node.rb'
-require './lib/weight.rb'
-require 'rgl/adjacency'
-require 'rgl/dot'
-require 'rgl/adjacency'
-require 'rgl/traversal'
-require 'ruby-debug'
-
--- weight/test.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-require "./require_me.rb"
-
-initial_graph = [[1, 2, 10], [2, 3, 10], [2, 4, 10], [4, 5, 10], [6, 4, 10], [1, 6, 10]]
-graph     = Graph.new(initial_graph)
-new_node  = graph.append_vertex(5, 10)
-weighting = Weight.new(graph.graph)
-weighting.adjust_weights(new_node, true)
-weighting.graph.write_to_graphic_file('jpg')
