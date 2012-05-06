8a26f89c9721660d409d77aed9d477e3314c2eab
jackdreilly <jackdreilly@gmail.com>
2012-05-05 20:08:15 -0700

    Update sprint/sprint/loans/models.py

--- sprint/sprint/loans/models.py
+++ sprint/sprint/loans/models.py
@@ -6,10 +6,16 @@ from endorsenet.models import EndorseNode
 
 class BorrowerModel(EndorseNode, iAgent.CreditBorrower):
     """ uses credit score """
+    # subclasses the EndorseNode, which means it can participate in network, 
+    # additionally is assumed to have external information in the form of a credit score
     credit_score_field = models.FloatField()
     
 class FunderModel(EndorseNode, iAgent.Funder):
-    """ uses credit score """
+    # These special case classes are important for generalization down the road
+    # as can be seen in LoanModel, each node need only be endowed with certain properties
+    # specific to funding and borrowing.
+    # of course, this can be superceded by implementing every EndorseNode as an AgentModel,
+    # since it inherits from all special cases.
     
     def will_fund(self, loan):
         # TODO: Implement
