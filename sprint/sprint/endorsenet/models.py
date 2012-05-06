f53c529b35f3cdf0814f4a72b59d8e95dfa0f55a
jackdreilly <jackdreilly@gmail.com>
2012-05-05 20:04:37 -0700

    Update sprint/sprint/endorsenet/models.py

--- sprint/sprint/endorsenet/models.py
+++ sprint/sprint/endorsenet/models.py
@@ -7,11 +7,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class EndorseNode(User, Endorser):
+    # here I have assumed that any endorser must have a full account
     pass
     
 class EndorseEdge(models.Model, Endorsement):
+    # I do not use this yet, but something should inherit the Endorsement interface
     pass
     
 class EndorserNetwork(iNetwork):
+    # this will be the singleton that the algorithm utilizes
     """ note that endorsers may endorse non endorsers (eg loans), this should be accounted for """
     pass
