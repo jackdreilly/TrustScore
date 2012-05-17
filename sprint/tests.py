62028d0128ea60fbd23f70debf006928449954e0
Jack Reilly <jackdreilly@gmail.com>
2012-05-16 19:51:58 -0700

    test script

--- sprint/tests.py
+++ sprint/tests.py
@@ -1,16 +1,33 @@
 from sprint.endorsenet.models import *
 from sprint.loans.models import *
+import random
 
-net = get_network()
+names = ('jack','jill','bill','ned', 'jane')
 
-nodes = net.nodes()
+agents = []
+for name in names:
+    agent = AgentModel()
+    agent.username = name
+    agent.password = name
+    agent.credit_score_field = random.randint(10,1000)
+    agent.save()
+    agents.append(agent)
 
-#jack = nodes.filter('username','jack')[0]
-#john = nodes.filter('username','john')[0]
-jack = nodes[0]
-john = nodes[1]
+for agent in agents:
+    print agent.username
+    agent.save()
+    
+    
+endorsers = random.sample(agents,4)
+endorsees = random.sample(agents,4)
+
+for er, ee in zip(endorsers,endorsees):
+    endorsement = er.endorsement(ee)
+    endorsement.save()
+    
+loans = [LoanModel(borrower = random.choice(agents), amount = random.randint(10,100)) for _ in range(10)]
 
 
-ends = EndorseEdge.objects.all()
-loan = LoanModel.objects.all()[0]
-
+for loan in loans:
+    loan.save()
+    
