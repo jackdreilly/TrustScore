841f2675458172cec03d018f6bfb55c7ea6942a3
Nathan Akers <nate.akers@gmail.com>
2012-07-25 22:12:22 -0700

    ux changes. layout more finalized, tables edited, pages made in PHP. needs to be re-translated to Python.

---
+++ sprint/sprint/demo/templates/nate/index.php
@@ -0,0 +1,17 @@
+<?php
+	// Include the header - site is setup in this file
+	include( 'include-header.php' );
+?>
+
+<?php
+	// Include the 'tab' pages
+	include( 'page-actionitems.php' );
+	include( 'page-agents.php' );
+	include( 'page-loans.php' );
+	include( 'page-searchresults.php' );
+?>
+
+<?php
+	//include footer - site & content is closed here.
+	include( 'include-footer.php' );
+?>
---
+++ sprint/sprint/demo/templates/nate/page-searchresults.php
@@ -0,0 +1,4 @@
+	<section class="tab-pane" id="tab4">
+		<h2>Search Results</h2>
+		<p>No Search Results Yet</p>
+	</section>
