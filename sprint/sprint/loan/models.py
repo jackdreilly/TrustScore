a46e64cc5606b307d6e09ac0a9afd5816110d47d
Jack Reilly <jackdreilly@gmail.com>
2012-08-04 16:45:19 -0700

    fixing trust models, default values added for floor, ceiling

--- sprint/sprint/loan/models.py
+++ sprint/sprint/loan/models.py
@@ -18,21 +18,27 @@ class Loan(t_models.TrustAction):
         return (math.exp(amount / self.amount) - 1) / (math.exp(1.0) - 1)
 
     def payment_floor_factor(self, amount): 
-        return self.floor_fn(self.accounted + amount) - self.floor_fn(self.accounted)
+        return self.floor_fn(self.accounted_amount + amount) - self.floor_fn(self.accounted_amount)
 
     def ceiling_fn(self, amount):
         return amount / self.trust_ceiling
 
     def payment_ceiling_factor(self, amount):
-        return self.ceiling_fn(self.accounted + amount) - self.ceiling_fn(self.accounted)
+        return self.ceiling_fn(self.accounted_amount + amount) - self.ceiling_fn(self.accounted_amount)
         
     def new_payment(self, amount):
         loan = self
-        floor = self.payment_floor_factor(amount) * self.trust_floor
-        ceiling = self.payment_ceiling_factor(amount) * self.trust_ceiling
+        floor = self.floor_from_amount(amount)
+        ceiling = self.ceiling_from_amount(amount) 
         payment = Payment(loan=loan, amount=amount, floor=floor, ceiling=ceiling)
         self.add_new_payment(payment)
         return payment
+
+    def floor_from_amount(self, amount):
+        return self.payment_floor_factor(amount) * self.trust_floor
+
+    def ceiling_from_amount(self, amount):
+        return self.payment_ceiling_factor(amount) * self.trust_ceiling
         
     def default(self):
         default = LoanDefaultEvent(loan = self)
@@ -74,7 +80,8 @@ class Loan(t_models.TrustAction):
         ]
         
     def all_payments(self):
-        return self.payment_set.all()
+        print dir(self)
+        return self.payments.all()
         
     def is_active(self):
         return self.amount_paid() >= self.amount
@@ -85,6 +92,7 @@ class Loan(t_models.TrustAction):
             for payment in self.all_payments()
         )
         
+    @property
     def accounted_amount(self):
         return sum(
             payment.amount
@@ -94,13 +102,26 @@ class Loan(t_models.TrustAction):
 class Payment(models.Model):
     loan = models.ForeignKey(Loan, related_name="payments")
     amount = models.FloatField()
-    floor = models.FloatField()
-    ceiling = models.FloatField()
+    floor = models.FloatField(blank=True)
+    ceiling = models.FloatField(blank=True)
     due_date = models.DateTimeField(auto_now_add=True)
 
     DEFAULT_WEIGHT = 2.0
     PAID_DECAY_RATE = 10.0
     MISSED_DECAY_RATE = 2.0
+
+    def save(self, *args, **kwargs):
+        if not self.floor:
+            self.floor = self.calculate_floor()
+        if not self.ceiling:
+            self.ceiling = self.calculate_ceiling()
+        super(Payment, self).save(*args, **kwargs)
+
+    def calculate_floor(self):
+        return self.loan.floor_from_amount(self.amount)
+
+    def calculate_ceiling(self):
+        return self.loan.ceiling_from_amount(self.amount)
     
     def defaulted(self):
         return len(self.default_events()) > 0
--- sprint/sprint/trust/models.py
+++ sprint/sprint/trust/models.py
@@ -6,9 +6,10 @@ class TrustActor(e_models.Actor):
     trust_score = models.FloatField(default=DEFAULT_SCORE)
 
 class TrustAction(e_models.Subject):
+	creation_date = models.DateTimeField(auto_now_add=True)
     actor = models.ForeignKey(TrustActor, related_name='actions')
     
 class TrustEvent(models.Model):
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     action = models.ForeignKey(TrustAction)
     score = models.FloatField()
